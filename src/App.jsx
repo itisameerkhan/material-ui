c0bef2711854c14637d2024b11e0455ff875083d
itisameerkhan <itisameerkhan@gmail.com>
2023-10-06 12:22:30 +0530

    06/10/2023 12:22

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import { Button } from "@mui/material";
+
 const App = () => {
   return (
     <div className="app-container">
-      <h1>hllo</h1>
+      <h1>hello</h1>
+      <Button variant="contained">Button</Button>
     </div>
   )
 };
