542fe37c5f36d3e44e549a67de00684bcaebf368
itisameerkhan <itisameerkhan@gmail.com>
2023-10-07 07:22:04 +0530

    07/10/2023 07:22

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ const App = () => {
     <div className="app-container">
       <h1>hello</h1>
       <Button variant="contained">Button</Button>
+      <Button variant="outlined">Button</Button>
     </div>
   )
 };
